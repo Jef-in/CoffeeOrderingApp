2bcd1e9d899b6699ec7cbad4e4beb97c7ef8ac6a
jefinabduljaleel <jefinabduljaleel@gmail.com>
2020-04-12 13:46:28 +0530

    Design Basic Completed Webservice Class and Load Function Creating Models

---
+++ CofeeApp/Controllers/AddOrderController.swift
@@ -0,0 +1,16 @@
+//
+//  AddOrderController.swift
+//  CofeeApp
+//
+//  Created by Jefin on 12/04/20.
+//  Copyright © 2020 Jefin. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AddOrderController : UIViewController {
+    
+    
+    
+}
